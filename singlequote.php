dc88cc843c608d9bc73f7eae6c5bbeb36375455b
sushmanth113 <86585994+sushmanth113@users.noreply.github.com>
2021-08-06 18:05:54 +0530

    Add files via upload

---
+++ singlequote.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'Hi i am sushmanth';
+echo 'hello i am working at aspire 
+systems';
+echo'hi i like "football":"I\'will be back"';
+echo 'You deleted c:\\*.*?';
+echo 'You deleted c:\*.*?';
+echo 'This will not expand \n new line ';
+echo 'The output do not $expand $either';
+echo "the output \v hello"
+?>
